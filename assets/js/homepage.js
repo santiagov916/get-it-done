8293d5efbe6a969c8ab968e7ca736edf081bc0a2
santiago <72000936+santiagov916@users.noreply.github.com>
2021-11-17 02:43:20 -0800

    updated commit

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,72 +3,84 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
-// this will take the username the user types into the HTML form verify it occupied and not empty. it store it inside of the username variable then pass it to the getUserRepos as an argumuent, it will then empty the value of the input for the next search.
+
+
 var formSubmitHandler = function(event) {
     event.preventDefault();
 
     //get value from input element
     var username = nameInputEl.value.trim();
 
-    if(username) {
+    if (username) {
         getUserRepos(username);
         nameInputEl.value = "";
     } else {
-        alert("Please enter a Github username");
+        alert("Please enter a Github Username.");
     }
     console.log(event);
 };
 
 var displayRepos = function(repos, searchTerm) {
-    //clear old content
-    repoContainerEl.textContent = "";
-    repoSearchTerm.textContent = searchTerm;
 
-    //loop over repos
-    for(var i =0; i < repos.length; i++) {
-        //format repo name
-        var repoName = repos[i].owner.login + "/" +repos[i].name;
+//check api for repos returned 
+if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+}
+   // loop over repos
+for (var i = 0; i < repos.length; i++) {
+    // format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+  
+    // create a container for each repo
+    var repoEl = document.createElement("div");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+  
+    // create a span element to hold repository name
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+  
+    // append to container
+    repoEl.appendChild(titleEl);
+  
+    // create a status element
+var statusEl = document.createElement("span");
+statusEl.classList = "flex-row align-center";
 
-        //create a container for each repo
-        var repoEl = document.createElement("div");
-        repoEl.classList = "list-item flex-row justify-space-betwee align-center";
-
-        //create a san element to hold repository name
-        var titleEL = document.createElement("span");
-        titleEL.textContent = repoName;
-
-        //append to container
-        repoEl.appendChild(titleEL);
-
-        // create a status element
-        var statusEl = document.createElement("span");
-        statusEl.classList = "flex-row align-center";
-
-        // check if current repo has issues or not
-        if (repos[i].open_issues_count > 0) {
-        statusEl.innerHTML =
-            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-        } else {
-        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-        }
+// check if current repo has issues or not
+if (repos[i].open_issues_count > 0) {
+  statusEl.innerHTML =
+    "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+} else {
+  statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+}
 
 // append to container
 repoEl.appendChild(statusEl);
 
-        //append container to the dom
-        repoContainerEl.appendChild(repoEl);
-    }
-};
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
+  }
+}
 
 var getUserRepos = function(user) {
-    //format the github api url
+    // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
-   
-   //make a request to the url
-        fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) { 
-            displayRepos(data, user);
-        });
-        });
-    };
+  
+    // make a request to the url
+    fetch(apiUrl)
+    .then(function(response) {
+        //request was successful
+        if (response.ok) {
+            response.json().then(function(data) {
+              displayRepos(data, user);
+      });
+    } else {
+        alert("Error: Github User Not Found");
+    }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to Github");
+    })
+  };
+
 userFormEl.addEventListener("submit", formSubmitHandler);
